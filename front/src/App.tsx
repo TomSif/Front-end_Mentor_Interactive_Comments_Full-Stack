584888f538dfb79468f4752077fdd8d51c35fbd2
TomSif <thomas.sifferle.programmer@gmail.com>
2026-06-18 09:25:11 +0200

    feat(app): load initial comments and user data

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,4 +1,25 @@
+import { useState, useEffect } from 'react'
+import { User, Comment } from '@/types'
+import getCommentsData from '@/services/comments'
+
 function App() {
+  const [currentUser, setCurrentUser] = useState<User>()
+  const [comments, setComments] = useState<Comment[]>([])
+  const [error, setError] = useState<string>('')
+
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const data = await getCommentsData()
+        setCurrentUser(data.currentUser)
+        setComments(data.comments)
+      } catch (err) {
+        if (err instanceof Error) setError(err.message)
+      }
+    }
+    loadData()
+  }, [])
+
   return (
     <main className="min-h-screen bg-[#F5F6FA]">
       <h1 className="text-center text-xl font-bold text-[#334253]">
