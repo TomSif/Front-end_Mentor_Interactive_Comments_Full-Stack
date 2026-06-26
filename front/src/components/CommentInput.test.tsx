d337e3c6a97441297d719a1074ac5f0378f4c838
TomSif <thomas.sifferle.programmer@gmail.com>
2026-06-26 08:34:38 +0200

    fix(test): clear mock before each test

--- front/src/components/CommentInput.test.tsx
+++ front/src/components/CommentInput.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react'
-import { describe, expect, it, vi } from 'vitest'
+import { describe, expect, it, vi, beforeEach } from 'vitest'
 import userEvent from '@testing-library/user-event'
 import CommentInput from './CommentInput'
 
@@ -13,6 +13,9 @@ const currentUser = {
 const handleSubmit = vi.fn()
 
 describe('Comment Input', () => {
+  beforeEach(() => {
+    handleSubmit.mockClear()
+  })
   it('renders the send button', () => {
     render(
       <CommentInput
